0406c2d61b044cc91415fa041d24c812840f5a8a
Joseph Infante <josephinfante.y@gmail.com>
2024-11-30 13:05:07 -0500

    creating spring project

---
+++ src/test/java/com/tendrylooks/daw/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tendrylooks.daw.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
