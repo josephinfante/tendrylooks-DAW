171cfc0144afdc93ffda6af1e8e3439c3c925902
Joseph Infante <josephinfante.y@gmail.com>
2024-12-14 15:27:05 -0500

    creating user controller

---
+++ src/main/java/com/tendrylooks/daw/main/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.tendrylooks.daw.main.controller;
+
+import com.tendrylooks.daw.main.dto.ApiResponseDto;
+import com.tendrylooks.daw.main.dto.LoginDto;
+import com.tendrylooks.daw.main.dto.UserCreateDto;
+import com.tendrylooks.daw.main.entity.User;
+import com.tendrylooks.daw.main.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponseDto> createUser(@RequestBody UserCreateDto userCreateDto) {
+        try {
+            userService.createUser(userCreateDto);
+            return ResponseEntity.ok(new ApiResponseDto("User created successfully"));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new ApiResponseDto("Error creating user: " + e.getMessage()));
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponseDto> login(@RequestBody LoginDto loginDto) {
+        try {
+            Optional<User> usuarioOpt = userService.verifyCredentials(loginDto);
+            if (usuarioOpt.isPresent()) {
+                String token = UUID.randomUUID().toString();
+                return ResponseEntity.ok().header("token", token).body(new ApiResponseDto("Login successful"));
+            }
+            return ResponseEntity.status(403).body(new ApiResponseDto("Invalid credentials. Please try again"));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(new ApiResponseDto("Error loging user: " + e.getMessage()));
+        }
+    }
+}
